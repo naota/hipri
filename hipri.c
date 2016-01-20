210ebaed30d5bceacf380932439c4a13e35f3ee2
Stephen Bates <stephen.bates@pmcs.com>
2016-01-20 05:51:02 -0700

    Added pthread testing of a mix of low and high priority IO.

--- hipri.c
+++ hipri.c
@@ -44,6 +44,18 @@
 
 #include <linux/types.h>
 
+#ifdef CONFIG_PREADV2
+
+  /*
+   * Note that since for now the SYS_preadv2 macro does not exist in
+   * glibc we hard-code it to the value in
+   * arch/x86/entry/syscalls/syscall_64.tbl. Livin' on the edge!!
+   */
+#define SYS_preadv2  326
+#define SYS_pwritev2 327
+
+#endif
+
   /*
    * A hack for now as I seem to have issues getting to the uapi
    * fs.h. Will fix this once I get this resolved.
@@ -53,9 +65,14 @@
 struct thread_info {
   pthread_t      thread_id;
   int            thread_num;
+
+  unsigned long  fd;
+  unsigned long  len;
+  char           filename[256];
+  struct iovec   *vec;
+  unsigned long  flags;
   struct timeval starttime;
   struct timeval endtime;
-  size_t         bytes;
 };
 
 static double timeval_to_secs(struct timeval *t)
@@ -73,16 +90,51 @@ static void report(struct timeval starttime, struct timeval endtime,
 	  bytes, elapsed_time);
 }
 
+static void run(struct thread_info *tinfo)
+{
+
+  tinfo->fd = open(tinfo->filename, O_RDWR | O_TRUNC |
+		   O_CREAT );
+  if ( tinfo->fd == -1 ){
+    fprintf(stderr,"error: open(): %s (%d)\n", strerror(errno),
+	    errno);
+    exit(errno);
+  }
+  gettimeofday(&tinfo->starttime, NULL);
+
+  tinfo->vec = malloc(sizeof(struct iovec));
+  if ( tinfo->vec == NULL ){
+    fprintf(stderr,"error: malloc(): %s (%d)\n", strerror(errno),
+	    errno);
+    exit(errno);
+  }
+  tinfo->vec->iov_base = malloc(tinfo->len);
+  tinfo->vec->iov_len  = tinfo->len;
+
+#ifdef CONFIG_PREADV2
+
+  syscall(SYS_pwritev2, tinfo->fd, tinfo->vec, 1, 0, 0,
+	  tinfo->flags);
+
+#else
+
+  syscall(SYS_pwritev, tinfo->fd, tinfo->vec, 1, 0, 0);
+
+#endif
+
+  gettimeofday(&tinfo->endtime, NULL);
+  free(tinfo->vec);
+  close(tinfo->fd);
+}
+
 static void *thread_start(void *arg)
 {
   struct thread_info *tinfo = arg;
 
-  gettimeofday(&tinfo->starttime, NULL);
   fprintf(stdout,"info: this is thread %d\n",
 	  tinfo->thread_num);
-  usleep(10000*(tinfo->thread_num+1));
-  gettimeofday(&tinfo->endtime, NULL);
-	  
+  run(tinfo);
+
   return 0;
 
 }
@@ -93,9 +145,6 @@ int main(int argc, char **argv)
   unsigned long fd, len = 8192, pos_l  = 0, pos_h = 0;
   struct iovec *vec;
   __u32 *buf, check;    
-#ifdef CONFIG_PREADV2
-  int flags = RWF_HIPRI;
-#endif
   
   if ( argc !=2 ){
     fprintf(stderr,"error: %s takes one argument!\n",
@@ -129,17 +178,9 @@ int main(int argc, char **argv)
   vec->iov_base = malloc(len);
   vec->iov_len  = len;
     
-    
 #ifdef CONFIG_PREADV2
 
-  /*
-   * Note that since for now the SYS_preadv2 macro does not exist in
-   * glibc we hard-code it to the value in
-   * arch/x86/entry/syscalls/syscall_64.tbl. Livin' on the edge!!
-   */
-
-#define SYS_preadv2 326  
-  syscall(SYS_preadv2, fd, vec, 1, pos_l, pos_h, flags);
+  syscall(SYS_preadv2, fd, vec, 1, pos_l, pos_h, RWF_HIPRI);
 
 #else
 
@@ -183,7 +224,12 @@ int main(int argc, char **argv)
    */
   struct thread_info tinfo[2];
   for ( unsigned i=0 ; i<2 ; i++){
+
     tinfo[i].thread_num = i;
+    tinfo[i].len        = (1<<24);
+    tinfo[i].flags = (i == 1) ? RWF_HIPRI : 0;
+    sprintf(tinfo[i].filename, "test%d.dat", i);
+
     if ( pthread_create(&tinfo[i].thread_id, NULL, thread_start, &tinfo[i]) ){
       fprintf(stderr,"error: pthread_create(): %s (%d)\n", strerror(errno),
 	      errno);
@@ -195,8 +241,8 @@ int main(int argc, char **argv)
     pthread_join(tinfo[i].thread_id, NULL);
   }
 
-  report(tinfo[0].starttime, tinfo[0].endtime, tinfo[0].bytes);
-  report(tinfo[1].starttime, tinfo[1].endtime, tinfo[1].bytes);
+  report(tinfo[0].starttime, tinfo[0].endtime, 2*tinfo[0].len);
+  report(tinfo[1].starttime, tinfo[1].endtime, 2*tinfo[1].len);
 
   free(vec->iov_base);
   free(vec);
